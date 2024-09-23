0cfe515453dd450c591f783f519a0702036b8ce5
swszz <swsz@kakao.com>
2024-09-23 22:54:25 +0900

    feat: R extends Any

--- src/main/kotlin/org/github/commons4k/collectionExtensions.kt
+++ src/main/kotlin/org/github/commons4k/collectionExtensions.kt
@@ -2,6 +2,6 @@ package org.github.commons4k
 
 import kotlin.reflect.KProperty1
 
-fun <T, R> List<T>.filter(property: KProperty1<T, R>, value: R): List<T> {
+fun <T, R : Any> List<T>.filter(property: KProperty1<T, R>, value: R): List<T> {
     return this.filter { item -> property.get(item) == value }
 }
